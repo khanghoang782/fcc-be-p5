b13b443ecb7137cf06b8875a67fa29237296ad57
khanghoang782 <pnhkhang92vp2@gmail.com>
2024-08-23 21:42:51 +0700

    Submit

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
-
+const multer = require('multer');
+const upload = multer({dest:'uploads/'});
 var app = express();
 
 app.use(cors());
@@ -10,7 +11,13 @@ app.use('/public', express.static(process.cwd() + '/public'));
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
-
+app.post('/api/fileanalyse',upload.single('upfile'),(req,res)=>{
+  res.json({
+    name:req.file.originalname,
+    type:req.file.mimetype,
+    size:req.file.size
+  });
+});
 
 
 
